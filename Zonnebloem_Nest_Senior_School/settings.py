41364952f024c14c3e2cd0ab69c9acd715d82ded
Coder2100 <apexcurestats@gmail.com>
2019-07-08 18:50:48 +0200

    UI

--- Zonnebloem_Nest_Senior_School/settings.py
+++ Zonnebloem_Nest_Senior_School/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Welcome.apps.WelcomeConfig',
-    'CareerAdvice.app.CareerAdvice',
+    'CareerAdvice.apps.CareeradviceConfig',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #static media files
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
